f57735a4313f2d261790ef430da5e7f2de83898a
ramandeep singh <raman0833@yahoo.com>
2018-07-08 12:44:35 +0530

    initialize project

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Javascript Practice</title>
+	<script type="text/javascript">
+		var a = 10;
+		var b = '20';
+		// var x = [1,2,3,4,5,6];
+		var x = [];
+		x['test'] = 1;
+		x['new'] = 2;
+
+		var y = {test:1,hey:2};
+
+		var arrayOfArrays = [[1,2,3,4],[1,2,3,4,],[1,2,3,4],[1,2,3,4]];
+		var arrayOfObjects = [{one:1,two:2,three:3},{one:1,two:2,three:3},{one:1,two:2,three:3}];
+		var objectOfArrays = {one:[1,2,3,4],two:[1,2,3,4],three:[1,2,3,4],four:[1,2,3,4]};
+		var objectOfObjects = {one:{one:1,two:2,three:3},two:{one:1,two:2,three:3},three:{one:1,two:2,three:3}};
+
+		if(a === 10 && b == 21){
+			console.log('yes');
+		}else{
+			console.log('no');
+		}
+
+		// console.log(ypeof(a));
+		// console.log(objectOfObjects);
+	</script>
+</head>
+<body>
+
+</body>
+</html>
---
+++ loop.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Javascript Loop</title>
+	<script type="text/javascript">
+		// var testArray = [1,2,3,4,5];
+		// console.log(testArray);
+		var newArray = [];
+		newArray['one'] = 1;
+		newArray['two'] = 2;
+		newArray['three'] = 3;
+		newArray['four'] = 4;
+		newArray['five'] = 5;
+		var arrayKeys = Object.keys(newArray);
+		for(var i = 0; i < arrayKeys.length; i++){
+			console.log(newArray[arrayKeys[i]]);
+		}
+	</script>
+</head>
+<body>
+
+</body>
+</html>
